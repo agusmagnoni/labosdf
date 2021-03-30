fd29c7ae0869fcc60fa0aa77b782bca432516560
Agustina Magnoni <39961631+agusmagnoni@users.noreply.github.com>
2021-03-30 15:59:04 -0300

    Update scipy_fft.py

--- scipy_fft.py
+++ scipy_fft.py
@@ -5,7 +5,7 @@ from scipy.fftpack import fft
 # Number of sample points
 N = 600
 # sample spacing
-T = 1.0 / 800.0
+T = 1.0 / N
 x = np.linspace(0.0, N*T, N)
 y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
 yf = fft(y)
